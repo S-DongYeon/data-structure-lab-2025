12a52725f54fcf49008811bb0cc314436ed8a7f9
S-DongYoen <2david2@naver.com>
2025-03-17 09:53:47 +0900

    Lec2_Findmax project added

---
+++ Lec2_Findmax/Lec2_Findmax/find_max.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+int FindArrayMax(int score[], int n)
+{
+	int tmp = score[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (score[i] > tmp) {
+			tmp = score[i];
+		}
+	}
+	return tmp;
+}
+
+int main()
+{
+	int score[5] = { 1, 10, 2, 4, 5 };
+	int out;
+	out = FindArrayMax(score, 5);
+	printf("result = %d", out);
+	return 0;
+}
